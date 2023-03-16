689db6cd702b2c0b546ded8e8f3abd7eed94212d
ghuseynova <ghuseynova.dev@gmail.com>
2023-03-17 00:55:30 +0400

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../src/assets/App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faYoutube } from "@fortawesome/free-brands-svg-icons";
+const App = () => {
+    const [background, changeColor] = useState("light");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const toGreen = () => changeColor("green-color");
+    const toRed = () => changeColor("red-color");
+    const toBlue = () => changeColor("blue-color");
+    const light = () => changeColor("light");
+
+    return (
+        <div className={`${background} div-style`}>
+            <div>
+                {background === "light" ? (
+                    <button onClick={toGreen} style={{ fontSize: "80px", color: "green" }}>
+                        <FontAwesomeIcon icon={faGithub} />
+                    </button>
+                ) : (
+                    <button onClick={light} style={{ fontSize: "75px" }}>
+                        <FontAwesomeIcon icon={faGithub} style={{ color: "blue" }} />
+                    </button>
+                )}
+            </div>
+            <div>
+                {background === "light" ? (
+                    <button onClick={toBlue} style={{ fontSize: "80px", color: "blue" }}>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </button>
+                ) : (
+                    <button onClick={light} style={{ fontSize: "75px" }}>
+                        <FontAwesomeIcon icon={faLinkedin} style={{ color: "red" }} />
+                    </button>
+                )}
+            </div>
+            <div>
+                {background === "light" ? (
+                    <button onClick={toRed} style={{ fontSize: "80px", color: "red" }}>
+                        <FontAwesomeIcon icon={faYoutube} />
+                    </button>
+                ) : (
+                    <button onClick={light} style={{ fontSize: "75px" }}>
+                        <FontAwesomeIcon icon={faYoutube} style={{ color: "green" }} />
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
